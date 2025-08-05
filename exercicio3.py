57083fd7845a8c64a6ad400821a31317051392a4
lucas teixeira <91092558+DRAVINCK@users.noreply.github.com>
2025-08-05 13:06:17 -0300

    Update exercicio3.py

--- exercicio3.py
+++ exercicio3.py
@@ -2,13 +2,14 @@
 #o Peça dois números e exiba a soma, subtração, multiplicação e divisão.
 #o Utilize condicionais para verificar divisão por zero.
 
-
 num1 = int(input("primeiro num: "))
 num2 = int(input("segundo num: "))
 
 print(f"soma: {num1 + num2}")
 print(f"sub: {num1 - num2}")
 print(f"mult: {num1 * num2}")
-print(f"div: {num1 % num2}")
 
-
+if num2 != 0:
+    print(f"Divisão: {num1 / num2}")
+else:
+    print("Divisão: Erro! Não é possível dividir por zero.")
