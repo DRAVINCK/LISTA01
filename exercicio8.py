c4f7ca98aadb5ce9f644725dc971106689b49401
lucas teixeira <91092558+DRAVINCK@users.noreply.github.com>
2025-08-05 15:48:03 -0300

    Update exercicio8.py

--- exercicio8.py
+++ exercicio8.py
@@ -1,18 +1,6 @@
-quantidade_alunos = int(input("Digite a quantidade de alunos: "))
-maior_nota = 0
-menor_nota = 10
-soma_notas = 0
+qtd = int(input("Quantidade de alunos: "))
+notas = [float(input(f"Nota do aluno {i+1}: ")) for i in range(qtd)]
 
-for i in range(quantidade_alunos):
-    nota = float(input(f"Digite a nota do aluno {i+1}: "))
-    soma_notas += nota
-    if nota > maior_nota:
-        maior_nota = nota
-    if nota < menor_nota:
-        menor_nota = nota
-
-media = soma_notas / quantidade_alunos
-
-print(f"Media da turma: {media}")
-print(f"Maior nota: {maior_nota}")
-print(f"Menor nota: {menor_nota}")
+print(f"Média: {sum(notas)/qtd}")
+print(f"Maior nota: {max(notas)}")
+print(f"Menor nota: {min(notas)}")
